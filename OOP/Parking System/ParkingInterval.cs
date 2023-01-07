7d67858e6f5ac0849970898297d4c734f4101bfe
stivan30 <spapanov@gmail.com>
2023-01-07 21:35:04 +0200

    Добавям файл Readme 2

---
+++ OOP/Parking System/ParkingInterval.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class ParkingInterval
+{
+    private ParkingSpot parkingSpot;
+    
+    public ParkingSpot ParkingSpot
+    {
+        get
+        {
+            //TODO: implement me
+            throw new NotImplementedException();
+        }
+
+        set
+        {
+            //TODO: implement me
+            throw new NotImplementedException();
+        }
+    }
+
+    private string registrationPlate;
+
+    public string RegistrationPlate
+    {
+        get
+        {
+            //TODO: implement me
+            throw new NotImplementedException();
+        }
+
+        set
+        {
+            //TODO: implement me
+            throw new NotImplementedException();
+        }
+    }
+
+    private int hoursParked;
+
+    public int HoursParked
+    {
+        get
+        {
+            //TODO: implement me
+            throw new NotImplementedException();
+        }
+
+        set
+        {
+            //TODO: implement me
+            throw new NotImplementedException();
+        }
+    }
+
+    public double Revenue
+    {
+        get
+        {
+            //TODO: Implement me
+            throw new NotImplementedException();
+        }
+    }
+
+    public ParkingInterval(ParkingSpot parkingSpot, string registrationPlate, int hoursParked)
+    {
+        //TODO: Implement me
+        throw new NotImplementedException();
+    }
+
+    public override string ToString()
+    {
+        //TODO: Implement me
+        throw new NotImplementedException();
+    }
+}
---
+++ OOP/Parking System/ParkingSpot.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public abstract class ParkingSpot
+{
+    private int id;
+    private bool occupied;
+    private string type;
+    private double price;
+    protected List<ParkingInterval> parkingIntervals;
+   
+    public int Id
+    {
+        get
+        {
+            return this.id;
+        }
+
+        set
+        {
+            this.id = value;
+        }
+    }
+
+    public bool Occupied
+    {
+        get
+        {
+            return this.occupied;
+        }
+
+        set
+        {
+            this.occupied = value;
+        }
+    }
+
+    public string Type
+    {
+        get
+        {
+            return this.type;
+        }
+
+        set
+        {
+            this.type = value;
+        }
+
+    }
+
+    public double Price
+    {
+        get
+        {
+            return this.price;
+        }
+
+        set
+        {
+            if (value<=0)
+            {
+                throw new AccessViolationException("Parking price cannot be less or equal to 0!");
+            }
+            this.price = value;
+        }
+    }
+
+    public ParkingSpot(int id, bool occupied, string type, double price)
+    {
+        this.Id = id;
+        this.Occupied = occupied;
+        this.Type = type;
+        this.Price = price;
+        this.parkingIntervals = new List<ParkingInterval>();
+    }
+    
+    public virtual bool ParkVehicle(string registrationPlate, int hoursParked, string type)
+    {
+        //TODO: implement me
+        throw new NotImplementedException();
+    }
+
+    public List<ParkingInterval> GetAllParkingIntervalsByRegistrationPlate(string registrationPlate)
+    {
+        //TODO: implement me
+        throw new NotImplementedException();
+    }
+
+    public virtual double CalculateTotal()
+    {
+        //TODO: implement me
+        throw new NotImplementedException();
+    }
+
+    public override string ToString()
+    {
+        //TODO: implement me
+        throw new NotImplementedException();
+    }
+
+}
